8d7341ba9fa274f1cd4b62a5be04cf56e83f2f9e
Diego Nobre <infobase.dnobre@terceiros.mongeralaegon.com.br>
2019-07-11 17:51:07 -0300

    criacao de blade e ajuste na view de listagem

--- resources/views/admin/clients/index.blade.php
+++ resources/views/admin/clients/index.blade.php
@@ -1,33 +1,38 @@
-<h1>Listagem de Clientes</h1>
-<br/><br/>
-<table border="1">
-    <thead>
-    <tr>
-        <th>ID</th>
-        <th>Nome</th>
-        <th>CNPJ/CPF</th>
-        <th>Data Nasc.</th>
-        <th>E-mail</th>
-        <th>Telefone</th>
-        <th>Sexo</th>
-        <th>Ação</th>
-    </tr>
-    </thead>
-    <tbody>
-    @foreach($clients as $client)
+@extends('layouts.layout')
+
+@section('content')
+
+    <h3>Listagem de Clientes</h3>
+    <br/><br/>
+    <table class="table table-striped">
+        <thead>
         <tr>
-            <td>{{$client->id}}</td>
-            <td>{{$client->name}}</td>
-            <td>{{$client->document_number}}</td>
-            <td>{{$client->date_birth}}</td>
-            <td>{{$client->email}}</td>
-            <td>{{$client->phone}}</td>
-            <td>{{$client->sex}}</td>
-            <td>
-
-            </td>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>CNPJ/CPF</th>
+            <th>Data Nasc.</th>
+            <th>E-mail</th>
+            <th>Telefone</th>
+            <th>Sexo</th>
+            <th>Ação</th>
         </tr>
-    @endforeach
-    </tbody>
+        </thead>
+        <tbody>
+        @foreach($clients as $client)
+            <tr>
+                <td>{{$client->id}}</td>
+                <td>{{$client->name}}</td>
+                <td>{{$client->document_number}}</td>
+                <td>{{$client->date_birth}}</td>
+                <td>{{$client->email}}</td>
+                <td>{{$client->phone}}</td>
+                <td>{{$client->sex}}</td>
+                <td>
 
-</table>
+                </td>
+            </tr>
+        @endforeach
+        </tbody>
+
+    </table>
+    @endsection
