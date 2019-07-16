17d789239298aaf7d34da440c77906cd28453d31
Diego Nobre <infobase.dnobre@terceiros.mongeralaegon.com.br>
2019-07-16 17:09:39 -0300

    atualizacao de dados e spoofing

--- resources/views/admin/clients/edit.blade.php
+++ resources/views/admin/clients/edit.blade.php
@@ -11,6 +11,7 @@
     @endif
     <form method="post" action="{{route('clients.update',['client' => $client->id])}}">
         {{csrf_field()}}
+        {{method_field('PUT')}}
         <div class="form-group">
             <label for="name">Nome</label>
             <input class="form-control" id="name" name="name" value="{{$client->name}}">
@@ -36,9 +37,9 @@
             <label for="marital_status">Estado Civil</label>
             <select class="form-control" name="marital_status" id="marital_status" value="{{$client->marital_status}}">
                 <option value="">Selecione o Estado Civil</option>
-                <option value="1">Solteiro</option>
-                <option value="2">Casado</option>
-                <option value="3">Divorciado</option>
+                <option value="1" {{$client->marital_status == 1 ? 'selected="selected"' : ''}}>Solteiro</option>
+                <option value="2" {{$client->marital_status == 2 ? 'selected="selected"' : ''}}>Casado</option>
+                <option value="3" {{$client->marital_status == 3 ? 'selected="selected"' : ''}}>Divorciado</option>
             </select>
         </div>
 
