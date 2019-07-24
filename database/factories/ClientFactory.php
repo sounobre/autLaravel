63811e2ffef90a3ebf5bfd9a5d059cb179265ac4
Diego Nobre <infobase.dnobre@terceiros.mongeralaegon.com.br>
2019-07-24 15:48:49 -0300

    dando erro ao rodar o seed

--- database/factories/ClientFactory.php
+++ database/factories/ClientFactory.php
@@ -11,13 +11,27 @@ $factory->define(Client::class, function (Faker $faker) {
     $cpfs = cpfs();
     return [
         'name'                  => $faker->name,
-        'document_number'       => $cpfs[array_rand($cpfs,1)],
         'email'                 => $faker->email,
         'phone'                 => $faker->phoneNumber,
-        'defaulter'             => rand(0,1),
+        'defaulter'             => rand(0,1)
+    ];
+});
+
+$factory->state(\App\Client::class, \App\Client::TYPE_INDIVIDUAL, function (Faker $faker) {
+    $cpfs = cpfs();
+    return [
+        'document_number'       => $cpfs[array_rand($cpfs,1)],
         'date_birth'            => $faker->date(),
         'sex'                   => rand(1,10) % 2 == 0 ? 'm' : 'f',
         'marital_status'        => rand(1,3),
-        'physical_disability'   => rand(1,10) % 2 == 0 ? $faker->word : null,
+        'physical_disability'   => rand(1,10) % 2 == 0 ? $faker->word : null
+    ];
+});
+
+$factory->state(\App\Client::class, \App\Client::TYPE_INDIVIDUAL, function (Faker $faker) {
+    $cnpjs = cnpjs();
+    return [
+        'document_number'       => $cnpjs[array_rand($cnpjs,1)],
+        'company_name'          => $faker->company
     ];
 });
--- database/migrations/2019_07_22_235704_alter_to_client_type_clients_table.php
+++ database/migrations/2019_07_22_235704_alter_to_client_type_clients_table.php
@@ -23,8 +23,6 @@ class AlterToClientTypeClientsTable extends Migration
             }, $maritalStatus);
             $maritalStatusEnum = implode(',', $maritalStatusString);
             \DB::statement("ALTER TABLE clients CHANGE COLUMN marital_status marital_status ENUM($maritalStatusEnum) NULL");
-            $table->char('sex', 10)->nullable()->change();
-            $table->enum('marital_status',array_keys(\App\Client::MARITAL_STATUS))->nullable()->change();
             $table->string('company_name')->nullable();
         });
     }
@@ -39,15 +37,13 @@ class AlterToClientTypeClientsTable extends Migration
         Schema::table('clients', function (Blueprint $table) {
             $table->dropUnique('clients_document_number_unique');
             $table->date('date_birth')->change();
-            \DB::statement('ALTER TABLE clients CHANGE COLUMN sex sex CHAR NOT NULL');
+            \DB::statement("ALTER TABLE clients CHANGE COLUMN sex sex CHAR NULL ");
             $maritalStatus = array_keys(\App\Client::MARITAL_STATUS);
             $maritalStatusString = array_map(function($value){
                 return "'$value'";
             }, $maritalStatus);
             $maritalStatusEnum = implode(',', $maritalStatusString);
-            \DB::statement("ALTER TABLE clients CHANGE COLUMN marital_status marital_status ENUM($maritalStatusEnum) NOT NULL");
-            $table->char('sex', 10)->change();
-            $table->enum('marital_status',array_keys(\App\Client::MARITAL_STATUS))->change();
+            \DB::statement("ALTER TABLE clients CHANGE COLUMN marital_status marital_status ENUM($maritalStatusEnum) NULL");
             $table->dropColumn('company_name');
         });
     }
